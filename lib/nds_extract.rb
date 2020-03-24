38d15ffd6fd5f3279416899032c66f74d337c978
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-24 04:02:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,10 +46,6 @@ def movies_with_directors_set(source)
   
 end
 
-# ----------------    End of Your Code Region --------------------
-# Don't edit the following code! Make the methods above work with this method
-# call code. You'll have to "see-saw" to get this to work!
-
 def studios_totals(nds)
   a_o_a_movies_with_director_names = movies_with_directors_set(nds)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
